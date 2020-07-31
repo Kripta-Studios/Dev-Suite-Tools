070dddc3630c8c0f38d0f2bf1e5e84d2b776f4ed
Kripta-Studios <68393696+Kripta-Studios@users.noreply.github.com>
2020-07-31 17:15:51 +0200

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,4 +1,4 @@
-sudo chmod a+x devSuite.py && sudo chmod a+x update.sh && sudo chmod a+x unist000.sh
+sudo chmod a+x devSuite.py && sudo chmod a+x update.sh && sudo chmod a+x uninst000.sh
 sudo cp devSuite.py devsuite
 sudo mv devsuite /usr/bin/
 sudo cp installAll.py /usr/bin/ && sudo cp installBasics.py /usr/bin/ && sudo cp lonePrograms.py /usr/bin/
