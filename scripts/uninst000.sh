6e5221133888bf97a44fb95be902dc047a50f7a3
Kripta-Studios <68393696+Kripta-Studios@users.noreply.github.com>
2020-07-31 17:10:52 +0200

    Finishing the unist000.sh file and system

--- scripts/uninst000.sh
+++ scripts/uninst000.sh
@@ -1,1 +1,7 @@
-
+ cd ../../
+sudo rm -rf Dev-Suite-Tools/
+sudo rm /usr/bin/installAll.py
+sudo rm /usr/bin/lonePrograms.py
+sudo rm /usr/bin/installBasics.py
+sudo rm /usr/bin/devsuite
+echo Dev-Suite-Tools unistalled succesfully.
