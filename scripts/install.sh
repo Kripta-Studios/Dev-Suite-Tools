3d24507192c06f085b9500bf457688748026ce55
Kripta-Studios <68393696+Kripta-Studios@users.noreply.github.com>
2020-08-06 12:02:24 +0200

    Delete install.sh

--- scripts/install.sh
+++
@@ -1,5 +0,0 @@
-sudo chmod a+x devSuite.py && sudo chmod a+x update.sh && sudo chmod a+x uninst000.sh
-sudo cp devSuite.py devsuite
-sudo mv devsuite /usr/bin/
-sudo cp installAll.py /usr/bin/ && sudo cp installBasics.py /usr/bin/ && sudo cp lonePrograms.py /usr/bin/
-echo To execute use devsuite install <pkg>
